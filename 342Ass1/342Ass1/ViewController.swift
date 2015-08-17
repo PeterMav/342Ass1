f7f94309ae1c0f613462d2d4ffd8eccafe8296d0
PeterMav <peter.mavo@gmail.com>
2015-08-17 13:25:45 +1000

    .

---
+++ 342Ass1/342Ass1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  342Ass1
+//
+//  Created by Peter Mavridis on 17/08/2015.
+//  Copyright (c) 2015 Peter Mavridis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
