0b733d43909a9adf8c499b3d0d3a3dd1a5b13cc2
PeterMav <peter.mavo@gmail.com>
2015-08-18 01:32:53 +1000

    .

--- 342Ass1/342Ass1/GameModel.swift
+++ 342Ass1/342Ass1/GameModel.swift
@@ -7,20 +7,86 @@
 //
 
 import Foundation
+import UIKit
 
-class GameModel: NSObject {
-    
-    struct TileData {
-        var tileImage = [String]()
-        var imageIdentifier: Int
+struct TileData {
+    var tileImage = [String]()
+    var imageIdentifier: Int
+    var imageArray = [String]()
+    init(){
+        imageIdentifier = 0;
+        imageArray += ["baldhill.png"]
+        imageArray += ["cathedral.png"]
+        imageArray += ["lake.png"]
+        imageArray += ["question.png"]
     }
-    
-    var lastTileTapped: Int
+}
+
+class GameModel: NSObject, Printable, DebugPrintable {
     var tileData = [TileData]()
+    var images = TileData()
+    var lastTileTapped: Int
+    var firstTouched: Bool
+    var totalTilesMatched: Int
+    var score: Int
+    var counter: Int
+    var j: Int
+    var k: Int
+    
+    func gameReset(){
+        counter = 0;
+        j = 9;
+        k = 0;
+        lastTileTapped = 0;
+        firstTouched = false
+        totalTilesMatched = 0
+        score = 0
+        // Loads the images and identifiers
+        for var i = 0; i < 9; i++ {
+            tileData[i].tileImage = images.imageArray[counter]
+            tileData[i].imageIdentifier = k
+            tileData[j].tileImage = images.imageArray[counter]
+            tileData[j].imageIdentifier = k
+            counter++
+            j++
+            k++
+            if counter == 3{
+                counter = 0
+                k = 0
+            }
+        }
+        tileData.shuffle() // The shuffle
+        printArray()
+    }
     
     override init () {
+        counter = 0;
+        j = 1;
+        k = 0;
         lastTileTapped = 0;
+        firstTouched = false
+        totalTilesMatched = 0
+        score = 0
+        super.init()
+        gameReset().self
     }
     
-    
+    func printArray(){
+        for var i = 0; i < 18; i++ {
+            println(tileData[i].tileImage)
+        }
+    }
+}
+
+
+// The shuffle
+extension Array
+{
+    mutating func shuffle()
+    {
+        for _ in 0..<18
+        {
+            sort { (_,_) in arc4random() < arc4random() }
+        }
+    }
 }
--- 342Ass1/342Ass1/ViewController.swift
+++ 342Ass1/342Ass1/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 var gameModel = GameModel()
-
+var tileData = TileData()
 class ViewController: UIViewController {
 
     @IBOutlet weak var tile1: UIImageView!
@@ -30,32 +30,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var tile16: UIImageView!
     @IBOutlet weak var tile17: UIImageView!
     @IBOutlet weak var tile18: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    
     
     override func viewDidLoad() {
+        gameModel.gameReset()
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        tile1.image = UIImage(named: "question.png")
-        tile2.image = UIImage(named: "question.png")
+       
+        tile1.image = UIImage(named: tileData.imageArray[3])
+        tile2.image = UIImage(named: tileData.imageArray[3])
+        tile3.image = UIImage(named: tileData.imageArray[3])
+        tile4.image = UIImage(named: tileData.imageArray[3])
+        tile5.image = UIImage(named: tileData.imageArray[3])
+        tile6.image = UIImage(named: tileData.imageArray[3])
+        tile7.image = UIImage(named: tileData.imageArray[3])
+        tile8.image = UIImage(named: tileData.imageArray[3])
+        tile9.image = UIImage(named: tileData.imageArray[3])
+        tile10.image = UIImage(named: tileData.imageArray[3])
+        tile11.image = UIImage(named: tileData.imageArray[3])
+        tile12.image = UIImage(named: tileData.imageArray[3])
+        tile13.image = UIImage(named: tileData.imageArray[3])
+        tile14.image = UIImage(named: tileData.imageArray[3])
+        tile15.image = UIImage(named: tileData.imageArray[3])
+        tile16.image = UIImage(named: tileData.imageArray[3])
+        tile17.image = UIImage(named: tileData.imageArray[3])
+        tile18.image = UIImage(named: tileData.imageArray[3])
 
-       // tile3.image = UIImage(named: "question.png")
-        /*
-        tile4.image = UIImage(named: "question.png")
-        tile5.image = UIImage(named: "question.png")
-        tile6.image = UIImage(named: "question.png")
-        tile7.image = UIImage(named: "question.png")
-        tile8.image = UIImage(named: "question.png")
-        tile9.image = UIImage(named: "question.png")
-        tile10.image = UIImage(named: "question.png")
-        tile11.image = UIImage(named: "question.png")
-        tile12.image = UIImage(named: "question.png")
-        tile13.image = UIImage(named: "question.png")
-        tile14.image = UIImage(named: "question.png")
-        tile15.image = UIImage(named: "question.png")
-        tile16.image = UIImage(named: "question.png")
-        tile17.image = UIImage(named: "question.png")
-        tile18.image = UIImage(named: "question.png")
-*/
     }
 
     override func didReceiveMemoryWarning() {
