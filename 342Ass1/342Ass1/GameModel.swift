3d8e0f7e48b97444889b81ee96e39563dab91794
PeterMav <peter.mavo@gmail.com>
2015-08-17 14:15:26 +1000

    Added the tiles to the storyboard and made the IBOutlets

---
+++ 342Ass1/342Ass1/GameModel.swift
@@ -0,0 +1,16 @@
+//
+//  GameModel.swift
+//  342Ass1
+//
+//  Created by Peter Mavridis on 17/08/2015.
+//  Copyright (c) 2015 Peter Mavridis. All rights reserved.
+//
+
+import Foundation
+
+class GameModel {
+    struct TileData {
+        var tileImage = [String]()
+        var imageIdentifier: Int
+    }
+}
--- 342Ass1/342Ass1/ViewController.swift
+++ 342Ass1/342Ass1/ViewController.swift
@@ -8,8 +8,28 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tile1: UIImageView!
+    @IBOutlet weak var tile2: UIImageView!
+    @IBOutlet weak var tile3: UIImageView!
+    @IBOutlet weak var tile4: UIImageView!
+    @IBOutlet weak var tile5: UIImageView!
+    @IBOutlet weak var tile6: UIImageView!
+    @IBOutlet weak var tile7: UIImageView!
+    @IBOutlet weak var tile8: UIImageView!
+    @IBOutlet weak var tile9: UIImageView!
+    @IBOutlet weak var tile10: UIImageView!
+    @IBOutlet weak var tile11: UIImageView!
+    @IBOutlet weak var tile12: UIImageView!
+    @IBOutlet weak var tile13: UIImageView!
+    @IBOutlet weak var tile14: UIImageView!
+    @IBOutlet weak var tile15: UIImageView!
+    @IBOutlet weak var tile16: UIImageView!
+    @IBOutlet weak var tile17: UIImageView!
+    @IBOutlet weak var tile18: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +39,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
