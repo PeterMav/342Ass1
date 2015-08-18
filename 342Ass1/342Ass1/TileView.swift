19379449495da6c746e5171a0d4bacddbbf9f6bd
PeterMav <peter@Peters-Mac-mini.local>
2015-08-19 04:44:41 +1000

    Part2
    Complete I think??

---
+++ 342Ass1/342Ass1/TileView.swift
@@ -0,0 +1,15 @@
+//
+//  TileView.swift
+//  342Ass1
+//
+//  Created by Peter Mavridis on 19/08/2015.
+//  Copyright (c) 2015 Peter Mavridis. All rights reserved.
+//
+
+import Foundation
+
+class TileView {
+    override init(coder aDecoder: NSCoder){
+        
+    }
+}
