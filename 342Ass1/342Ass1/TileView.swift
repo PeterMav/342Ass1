6e67d3adb6a92f9bfbe3a4a0a2b4da0fb4a4c075
PeterMav <peter@Peters-Mac-mini.local>
2015-08-19 15:31:45 +1000

    Loads Images!
    And constraints for the images to be in the tiles!

--- 342Ass1/342Ass1/TileView.swift
+++ 342Ass1/342Ass1/TileView.swift
@@ -14,20 +14,29 @@ class TileView: UIView {
     let imageView: UIImageView?
     var tileIndex: Int
     var innerView: UIView
+    var imageName: String?
+    
+    
+    
     
      required init(coder aDecoder: NSCoder){
-        
-        self.image = UIImage(named: "question.png")
-        self.imageView = UIImageView()
+        self.imageName = "question.png"
+        self.image = UIImage(named: imageName!)
+        self.imageView = UIImageView(image: image!)
         self.tileIndex = 0
         innerView = UIView()
         super.init(coder: aDecoder)
         
+        // Sets the background color of the tiles!
         innerView.backgroundColor = UIColor.purpleColor()
         self.backgroundColor = UIColor.purpleColor()
         self.addSubview(innerView)
         
-        image?.superclass
+        
+        // Sets the constraints to the images size and loads images to the tiles.
+        imageView?.frame = CGRect(x: 0, y: 0, width: 90, height: 80)
+        self.addSubview(imageView!)
+        
     }
     
     
