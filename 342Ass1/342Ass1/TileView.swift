3425d8b80cdd268c49b99ca650682c4ea6126da1
PeterMav <peter@Peters-Mac-mini.local>
2016-04-04 23:10:04 +1000

    .

--- 342Ass1/342Ass1/TileView.swift
+++ 342Ass1/342Ass1/TileView.swift
@@ -7,7 +7,37 @@
 //
 
 import Foundation
+import UIKit
 
-class TileView {
-    
+class TileView: UIView {
+    let image: UIImage?
+    let imageView: UIImageView?
+    var tileIndex: Int
+    var innerView: UIView
+    var imageName: String?
+    //let tileData = [TileData]()
+    let images = TileData()
+    required init?(coder aDecoder: NSCoder){
+        self.imageName = images.imageArray[3] // Sets all tiles to question.png
+        self.image = UIImage(named: imageName!)
+        self.imageView = UIImageView(image: image!)
+        self.tileIndex = 0
+        innerView = UIView()
+        super.init(coder: aDecoder)
+        
+        // Sets the background color of the tiles!
+        innerView.backgroundColor = UIColor.purpleColor()
+        self.backgroundColor = UIColor.purpleColor()
+        self.addSubview(innerView)
+        
+        
+        // Sets the constraints to the images size and loads images to the tiles.
+        imageView?.frame = CGRect(x: 0, y: 0, width: 90, height: 80)
+        
+        //self.addSubview(imageView!)
+        self.addSubview(imageView!)
+        
+        
+        
+    }
 }
